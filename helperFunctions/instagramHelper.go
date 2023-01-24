dff39a103ccf7ccb3562700414d03078982a2918
ali <mommy@pleasedrinkwater.local>
2023-01-24 11:49:14 +0900

    getting business account and ig account numbers working

--- helperFunctions/instagramHelper.go
+++ helperFunctions/instagramHelper.go
@@ -9,7 +9,7 @@ import (
 	"os"
 )
 
-type IgMeAccountStruct struct {
+type FBBusinessAccountInfo struct {
 	Data []struct {
 		AccessToken  string `json:"access_token"`
 		Category     string `json:"category"`
@@ -26,6 +26,13 @@ type IgMeAccountStruct struct {
 			After  string `json:"after"`
 		} `json:"cursors"`
 	} `json:"paging"`
+}
+
+type IGBusinessAccountInfo struct {
+	InstagramBusinessAccount struct {
+		ID string `json:"id"`
+	} `json:"instagram_business_account"`
+	ID string `json:"id"`
 }
 
 func InstagramActivate() {
@@ -48,13 +55,39 @@ func InstagramActivate() {
 	// log.Printf("respones body: %s", string(body))
 
 	//unpack json object
-	var igMeAccountInfo IgMeAccountStruct
-	marshallerr := json.Unmarshal(body, &igMeAccountInfo)
+	var fbBusinessAccountInfo FBBusinessAccountInfo
+	marshallerr := json.Unmarshal(body, &fbBusinessAccountInfo)
 	if marshallerr != nil {
 		log.Panic(marshallerr)
 	}
-	log.Printf("creator id struct %s", igMeAccountInfo)
-	log.Printf("creator id pulled %s", igMeAccountInfo.Data[0].ID)
+	businessId := fbBusinessAccountInfo.Data[0].ID
+	log.Printf("creator id struct %s", fbBusinessAccountInfo)
+	log.Printf("creator id pulled %s", businessId)
 	resp.Body.Close()
 
+	resp2, get2err := http.Get(fmt.Sprintf("https://graph.facebook.com/v15.0/%s?fields=instagram_business_account&access_token=%s", businessId, accessToken))
+
+	if get2err != nil {
+		log.Panic(get2err)
+	}
+	// // how to print out body example
+	// dump, dumpyerr := httputil.DumpResponse(resp2, true)
+	// if dumpyerr != nil {
+	// 	log.Panic(dumpyerr)
+	// }
+	// log.Printf("Response Body: %s", dump)
+
+	body2, body2readerr := io.ReadAll(resp2.Body)
+	if body2readerr != nil {
+		log.Panic(body2readerr)
+	}
+	log.Printf("respones body: %s", string(body2))
+
+	//unpack json object
+	var igBusnessAccountInfo IGBusinessAccountInfo
+	igError := json.Unmarshal(body2, &igBusnessAccountInfo)
+	if igError != nil {
+		log.Panic(igError)
+	}
+	log.Printf("igInfo %v", igBusnessAccountInfo)
 }
